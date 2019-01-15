615adbc7c46c980497f53ddda8af3210c55cde87
MrYeL <mryelee@163.com>
2019-01-15 09:46:58 +0800

    1、如何创建一个新工程

---
+++ iOS_Learning_Demo/iOS_Learning_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_Learning_Demo
+//
+//  Created by MrYeL on 2019/1/15.
+//  Copyright © 2019 MrYeL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_Learning_Demo/iOS_Learning_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_Learning_Demo
+//
+//  Created by MrYeL on 2019/1/15.
+//  Copyright © 2019 MrYeL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
